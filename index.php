5105e4f49fd00ee5ecef84c62ea9728cda260fca
Neurozone <5229035+Neurozone@users.noreply.github.com>
2020-04-05 18:32:29 +0200

    ajout de composer et de: - twig - bramus router

--- index.php
+++ index.php
@@ -1,1 +1,56 @@
 <?php
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require __DIR__ . '/vendor/autoload.php';
+
+function siteURL()
+{
+    if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+        $protocol = 'https://';
+    } else {
+        $protocol = 'http://';
+    }
+    $domainName = $_SERVER['HTTP_HOST'];
+    return $protocol . $domainName;
+}
+
+define('SITE_URL', siteURL());
+
+$templateName = 'default';
+$templatePath = __DIR__ . '/templates/' . $templateName;
+
+$loader = new \Twig\Loader\FilesystemLoader($templatePath);
+$twig = new \Twig\Environment($loader, ['cache' => __DIR__ . '/cache', 'debug' => true,]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$router = new \Bramus\Router\Router();
+
+$router->get('/', function () use ($twig) {
+
+
+    echo $twig->render('index.twig',
+        [
+            'base_url' => SITE_URL
+        ]
+    );
+
+});
+
+$router->run();
--- private/stats_log.php
+++
@@ -1,1 +0,0 @@
-<?php /* S7QysKquBQA= */ ?>
